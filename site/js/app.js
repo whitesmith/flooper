c474a84686e479076917a24ad63cc41e1e5cb51a
Tomás Correia Marques <tomasmanuelcorreiamarques@gmail.com>
2017-11-08 15:27:04 +0000

    add demo buttons

--- site/js/app.js
+++ site/js/app.js
@@ -2,3 +2,12 @@ console.log('hello world');
 
 var i = new Flooper();
 i.init();
+
+document.querySelector(".js-flooper-pause").onclick = () => {
+  i.pause();
+  console.log("paused");
+};
+document.querySelector(".js-flooper-resume").onclick = () => {
+  i.resume();
+  console.log("resumed");
+};
