bb853aac032f327ea0853522f0a000a62487dfb4
Tomás Correia Marques <tomasmanuelcorreiamarques@gmail.com>
2017-11-13 15:07:19 +0000

    add animation and pause logic

--- site/js/app.js
+++ site/js/app.js
@@ -1,16 +1,32 @@
 
 import Flooper from '../../dist/Flooper.js';
 import {consoleColophon} from './consoleColophon';
+import {toggleAnimation} from './utils';
 
 consoleColophon();
 var demoFlooper = new Flooper();
 demoFlooper.init();
 
+var paused = false;
+
 document.querySelector(".js-flooper-pause").onclick = () => {
   demoFlooper.pause();
+
+  if(!paused) toggleAnimation(document.querySelector(".js-flooper"), "paused");
+  paused=true;
+
   console.log("paused");
 };
 document.querySelector(".js-flooper-resume").onclick = () => {
   demoFlooper.resume();
+
+  if(paused) toggleAnimation(document.querySelector(".js-flooper"), "paused");
+  paused=false;
+
   console.log("resumed");
 };
+
+// Add random delay to floating animation
+document.querySelectorAll(".js-flooper-item").forEach( (e) => {
+  e.style.animationDelay = Math.random()*3 + "s";
+});
